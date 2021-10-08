8588b2b2d8593f80a4d96082971eda0f212bdef7
azharjk <azhr.project@gmail.com>
2021-10-08 19:24:22 +0700

    Randomize the answers

--- src/quiz.cpp
+++ src/quiz.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 #include <cassert>
 #include <string>
+#include <algorithm>
+#include <random>
+#include <chrono>
 
 struct Question {
   std::string text;
@@ -16,6 +19,10 @@ int main() {
   q1.answers.push_back("11");
   q1.answers.push_back("12");
   q1.correct_answer = "10";
+
+  auto seed = std::chrono::system_clock::now().time_since_epoch().count();
+  std::default_random_engine rng(seed);
+  std::shuffle(q1.answers.begin(), q1.answers.end(), rng);
 
   char answer_indicator = 'a';
   std::string client_answer;
@@ -40,13 +47,13 @@ int main() {
     }
   } while (client_answer.empty());
 
-  assert(client_answer.size() == 1);
-
   std::size_t answer_index = client_answer[0] - 97;
+  assert(answer_index < q1.answers.size());
+  char chosen_answer_indicator = 'a' + answer_index;
   if (q1.answers[answer_index] == q1.correct_answer) {
-    std::cout << "Your answer " << static_cast<char>('a' + answer_index) << " is correct\n";
+    std::cout << "Your answer " << chosen_answer_indicator << " is correct\n";
   } else {
-    std::cout << "Your answer " << static_cast<char>('a' + answer_index) << " is not correct\n";
+    std::cout << "Your answer " << chosen_answer_indicator << " is not correct\n";
   }
 
   return 0;
